02a6a98898abd228738b696ccec9da03c19538b3
emil584c <105641442+emil584c@users.noreply.github.com>
2023-04-30 15:25:51 +0200

    STRUCTED TEXT VIRKER OGSÅ

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import { request } from "../datocms";
+import { StructuredText as DatocmsStructuredText } from "vue-datocms";
 
 const data = ref(null);
 const error = ref(null);
@@ -8,24 +9,24 @@ const loading = ref(true);
 
 const HOMEPAGE_QUERY = `
   query HomePage {
-    homePage {
+    homePage{
       id
       name
-      heroContent {
-        ... on HeroRecord {
+      heroContent{
+        ... on HeroRecord{
           heroText
           heroDescription {value}
         }
       }
       aboutSection{
-      ... on AboutRecord{
-        sectionTitle
-        aboutText {value}
-        aboutImage {
-          id
+        ... on AboutRecord{
+          sectionTitle
+          aboutText {value}
+          aboutImage {
+            id
+          }
         }
       }
-  	}
     }
   }
 `;
@@ -46,8 +47,9 @@ request({ query: HOMEPAGE_QUERY }).then(result => {
     <div v-if="loading">Loading...</div>
     <div v-else-if="error">Something bad happened</div>
     <div v-else>
-      <h1> y{{ data.homePage.name }}</h1>
+      <h1>{{ data.homePage.name }}</h1>
       <p>{{ data.homePage.heroContent[0].heroText }}</p>
+      <datocms-structured-text :data="data.homePage.heroContent[0].heroDescription" />
     </div>
   </div>
 </template>
